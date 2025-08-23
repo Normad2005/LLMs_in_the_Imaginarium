4b6d8f12174361c0cf13be6d8c438715a8cc64f5
Haruka0126 <90200858+Normad2005@users.noreply.github.com>
2025-08-23 22:59:46 +0800

    Update real_api.py

--- real_api.py
+++ real_api.py
@@ -3,7 +3,7 @@
 import requests
 import wikipedia
 
-API_KEY = "01f863cd8a24c54dfe2042949f4d20e2"
+API_KEY = "api key"
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast"
 
